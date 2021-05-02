362cd0c3a0a960004db3fa55e77d9776db1dc3ed
pushkarraj <pushkarraj@pujari.me>
2021-05-02 16:55:45 +0530

    DONE : Made some optimizations in selection sort

--- src/main/java/me/pujari/algorithms/sorting/SelectionSort.java
+++ src/main/java/me/pujari/algorithms/sorting/SelectionSort.java
@@ -3,9 +3,9 @@ package me.pujari.algorithms.sorting;
 public class SelectionSort implements ISorting {
 
 	public Integer[] sort(Integer[] intArray) {
-		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex >= 0; lastUnsortedIndex--) {
+		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
 			int largestIntIndex = 0;
-			for (int currentIndex = 0; currentIndex <= lastUnsortedIndex; currentIndex++) {
+			for (int currentIndex = 1; currentIndex <= lastUnsortedIndex; currentIndex++) {
 				if (intArray[currentIndex] > intArray[largestIntIndex]) {
 					largestIntIndex = currentIndex;
 				}
