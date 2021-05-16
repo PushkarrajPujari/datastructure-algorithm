b3fc4c954143cc59919cc9be5db6b1cb1019b842
PushkarrajPujari <pushkarraj@pujari.me>
2021-05-16 16:49:02 +0530

    DONE : implemented circular queue

--- src/main/java/me/pujari/datastructure/queue/ACircularQueue.java
+++ src/main/java/me/pujari/datastructure/queue/ACircularQueue.java
@@ -1,17 +1,69 @@
 package me.pujari.datastructure.queue;
 
-public class ACircularQueue implements IQueue<Integer>{
-	
+public class ACircularQueue implements IQueue<Integer> {
+
+	private Integer[] intArray;
+	private int headPointer;
+	private int tailPointer;
+
+	public ACircularQueue(int size) {
+		this.intArray = new Integer[size];
+		this.headPointer = 0;
+		this.tailPointer = 0;
+	}
+
 	@Override
 	public IQueue<Integer> enqueue(Integer e) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!(tailPointer < headPointer) && tailPointer < intArray.length) {
+			intArray[tailPointer++] = e;
+		} else if (headPointer > 0 || tailPointer < headPointer) {
+			if (tailPointer == intArray.length) {
+				tailPointer = 0;
+			}
+			intArray[tailPointer] = e;
+			if ((tailPointer + 1) < headPointer) {
+				tailPointer++;
+			} else {
+				/**
+				 * Resize
+				 */
+				System.out.println("Queue is full, Resizing queue");
+				Integer[] newArray = new Integer[intArray.length + (intArray.length / 2)];
+				System.arraycopy(intArray, headPointer, newArray, 0, intArray.length - headPointer);
+				System.arraycopy(intArray, 0, newArray, intArray.length - headPointer, tailPointer + 1);
+				tailPointer = intArray.length;
+				headPointer = 0;
+				intArray = newArray;
+			}
+		} else {
+			/**
+			 * Resize
+			 */
+			System.out.println("Queue is full, Resizing queue");
+			Integer[] newArray = new Integer[intArray.length + (intArray.length / 2)];
+			System.arraycopy(intArray, 0, newArray, 0, intArray.length);
+			intArray = newArray;
+			enqueue(e);
+		}
+		return this;
 	}
 
 	@Override
 	public Integer dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		Integer value = null;
+		if (headPointer < intArray.length && headPointer != tailPointer) {
+			value = intArray[headPointer++];
+		} else if (headPointer > tailPointer) {
+			if(headPointer == intArray.length) {
+				headPointer = 0;
+			}
+			value = intArray[headPointer++];
+		} else {
+			System.out.println("Queue is empty");
+			headPointer = 0;
+			tailPointer = 0;
+		}
+		return value;
 	}
 
 }
---
+++ src/test/java/me/pujari/datastructure/queue/ACircularQueueTest.java
@@ -0,0 +1,27 @@
+package me.pujari.datastructure.queue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ACircularQueueTest {
+	@Test
+	void test() {
+		IQueue<Integer> queue= new ACircularQueue(17);
+		queue.enqueue(10).enqueue(20).enqueue(30);
+		assertEquals(10, queue.dequeue());
+		assertEquals(20, queue.dequeue());
+		assertEquals(30, queue.dequeue());
+		queue.enqueue(100).enqueue(200).enqueue(300);
+		assertEquals(100, queue.dequeue());
+		assertEquals(200, queue.dequeue());
+		assertEquals(300, queue.dequeue());
+		for(int i = 0;i<=15;i++) {
+			queue.enqueue(i);
+		}
+		for(int i =0 ;i<15;i++) {
+			assertEquals(i, queue.dequeue());
+		}
+
+	}
+}
